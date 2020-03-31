540253341e65f018b2de7983190ee2b2e01841e5
Randy Oest <oest@amazingrando.com>
2020-03-31 18:40:23 -0400

    help

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -10,14 +10,14 @@ const token = process.env.TOKEN;
 
 client.login(token);
 
-client.on('message', (message) => {
+client.on("message", message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).split(' ');
+  const args = message.content.slice(prefix.length).split(" ");
   const command = args.shift().toLowerCase();
   const modiferValue = parseInt(args[0], 10);
 
-  if (command === 'age' && modiferValue) {
+  if (command === "age" && modiferValue !== 0) {
     const d1 = Math.floor(Math.random() * 6) + 1;
     const d2 = Math.floor(Math.random() * 6) + 1;
     const dragon = Math.floor(Math.random() * 6) + 1;
@@ -31,19 +31,32 @@ client.on('message', (message) => {
       return obj;
     }, {});
 
-    const color = Object.keys(checkForDoubles).length <= 2 ? '#ff0000' : '#00b0f4';
-    const text = Object.keys(checkForDoubles).length <= 2 ? `You rolled **${d1 + d2 + dragon + modiferValue}**\nDoubles! Gain ${dragon} stunt points!` : `You rolled **${d1 + d2 + dragon + modiferValue}**`;
+    const color =
+      Object.keys(checkForDoubles).length <= 2 ? "#ff0000" : "#00b0f4";
+    const text =
+      Object.keys(checkForDoubles).length <= 2
+        ? `You rolled **${d1 +
+            d2 +
+            dragon +
+            modiferValue}**\nDoubles! Gain ${dragon} stunt points!`
+        : `You rolled **${d1 + d2 + dragon + modiferValue}**`;
 
     const embed = new Discord.MessageEmbed()
       .setColor(color)
       .setTitle(text)
-      .setDescription(`\`(${d1} + ${d2}) + (Dragon Die: ${dragon}) + ${modiferValue} = ${d1 + d2 + dragon + modiferValue}\``)
+      .setDescription(
+        `\`(${d1} + ${d2}) + (Dragon Die: ${dragon}) + ${modiferValue} = ${d1 +
+          d2 +
+          dragon +
+          modiferValue}\``
+      )
       .setTimestamp()
-      .setFooter('May the dice be ever in your favor');
+      .setFooter("May the dice be ever in your favor");
 
     message.channel.send(embed);
-  } else if (command === 'age' && args[0] === 'help') {
-    const helpText = 'To roll dice, enter the command `!age` followed by the modifier, such as `!age +2`, `!age 2`, or `!age -1`. You cannot change the prefix at this time.';
+  } else if (command === "age" && args[0] === "help") {
+    const helpText =
+      "To roll dice, enter the command `!age` followed by the modifier, such as `!age +2`, `!age 2`, or `!age -1`. You cannot change the prefix at this time.";
     message.channel.send(helpText);
   } else {
     const errorReply = `Error: make sure you are using a number. Ex: \`${prefix}age +2\`, \`${prefix}age 2\` or \`${prefix}age -1\``;
